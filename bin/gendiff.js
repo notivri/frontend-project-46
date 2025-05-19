092eeec4ea4872f50deb1be95ce0b3d299b64d1f
notivri <katsu4973@gmail.com>
2025-05-19 21:12:12 +0700

    Add help command arguments description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,10 @@ const program = new Command()
 
 program
   .name('gendiff')
+  .arguments('filepath1')
+  .arguments('filepath2')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .parse()
+  .option('-f, --format [type]', 'output format')
+
+program.parse()
