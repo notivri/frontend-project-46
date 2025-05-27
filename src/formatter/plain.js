0aed02d40a8e7a6bf0dc399584ff1d23dd82cd7d
notivri <katsu4973@gmail.com>
2025-05-27 14:16:23 +0700

    Add plain style support

--- src/formatter/plain.js
+++ src/formatter/plain.js
@@ -1,1 +1,29 @@
-export default function plain() {}
+import { isPlainObject } from '../utils.js';
+
+const stringify = (value) => {
+  if (isPlainObject(value)) return '[complex value]';
+  if (typeof value === 'string') return `'${value}'`;
+  return String(value);
+};
+
+export default function plain(changeData, path = '') {
+  const filtered = changeData.filter((item) => item.type !== 'unchanged');
+
+  return filtered
+    .map((data) => {
+      switch (data.type) {
+        case 'added': {
+          return `Property '${path}${data.key}' was added with value: ${stringify(data.value, data.key)}`;
+        }
+        case 'changed': {
+          return `Property '${path}${data.key}' was updated. From ${stringify(data.from)} to ${stringify(data.to, data.key)}`;
+        }
+        case 'nested': {
+          return plain(data.children, `${path}${data.key}.`);
+        }
+        default:
+          return `Property '${path}${data.key}' was removed`;
+      }
+    })
+    .join('\n');
+}
