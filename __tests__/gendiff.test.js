15d033c898add0660fffc0ddba4d743b3dbaaaf4
notivri <katsu4973@gmail.com>
2025-05-27 14:05:10 +0700

    Add test for plain format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,7 +11,7 @@ const YAMLfile1 = './__fixtures__/file1.yaml';
 const YAMLfile2 = './__fixtures__/file2.yml';
 
 const stylish = fs.readFileSync('__fixtures__/expected.stylish.txt', { encoding: 'utf-8' }).trim();
-// const plain = fs.readFileSync("__fixtures__/expected.plain.txt", { encoding: "utf-8" }).trim()
+const plain = fs.readFileSync('__fixtures__/expected.plain.txt', { encoding: 'utf-8' }).trim();
 
 it('should generate a diff between two files in stylish', () => {
   expect(genDiff(JSONfile1, JSONfile2)).toBe(stylish);
@@ -19,10 +19,10 @@ it('should generate a diff between two files in stylish', () => {
   expect(genDiff(YAMLfile1, YAMLfile2, 'stylish')).toBe(stylish);
 });
 
-// it('should generate a diff between two files in plain', () => {
-//   expect(genDiff(JSONfile1, JSONfile2, 'plain')).toBe(plain);
-//   expect(genDiff(YAMLfile1, YAMLfile2, 'plain')).toBe(plain);
-// });
+it('should generate a diff between two files in plain', () => {
+  expect(genDiff(JSONfile1, JSONfile2, 'plain')).toBe(plain);
+  expect(genDiff(YAMLfile1, YAMLfile2, 'plain')).toBe(plain);
+});
 
 it('should throw error for unknown format', () => {
   expect(() => parseFormat('nothing', 'some')).toThrow();
